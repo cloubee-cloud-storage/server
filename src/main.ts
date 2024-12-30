1a05883ed0a9541a5dab91c3fb69e5c32db71378
nayvok <44442827+nayvok@users.noreply.github.com>
2024-12-30 19:43:07 +0500

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { CoreModule } from './core/core.module';
 
@@ -7,6 +8,15 @@ async function bootstrap() {
     const app = await NestFactory.create(CoreModule);
 
     const config = app.get(ConfigService);
+
+    const swagger = new DocumentBuilder()
+        .setTitle('Cloubee')
+        .setVersion('1.0')
+        .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, swagger);
+
+    SwaggerModule.setup('swagger', app, documentFactory);
 
     app.enableCors({
         origin: config.getOrThrow<string>('ALLOWED_ORIGIN'),
