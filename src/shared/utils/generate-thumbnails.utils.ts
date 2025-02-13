f05bbefb5bfcde2aeb9cea18a9c0ad5d56987ccb
nayvok <44442827+nayvok@users.noreply.github.com>
2025-02-13 16:15:20 +0500

    feat: add generation of thumbnails for video files

--- src/shared/utils/generate-thumbnails.utils.ts
+++ src/shared/utils/generate-thumbnails.utils.ts
@@ -1,17 +1,44 @@
+import * as ffmpegInstaller from '@ffmpeg-installer/ffmpeg';
+import * as ffprobeInstaller from '@ffprobe-installer/ffprobe';
 import { ConfigService } from '@nestjs/config';
+import * as ffmpeg from 'fluent-ffmpeg';
+import * as fs from 'node:fs';
 import * as path from 'node:path';
 import * as sharp from 'sharp';
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+ffmpeg.setFfprobePath(ffprobeInstaller.path);
+
+const VIDEO_EXTENSIONS = [
+    '.mp4',
+    '.mov',
+    '.avi',
+    '.mkv',
+    '.webm',
+    '.flv',
+    '.wmv',
+    '.mpeg',
+    '.mpg',
+    '.3gp',
+    '.m4v',
+];
 
 export const generateThumbnails = async (
     userId: string,
     fileId: string,
-    imagePath: string,
+    filePath: string,
     config: ConfigService,
 ) => {
     try {
-        const fullImagePath = path.join(
+        let fullFilePath = path.join(
             config.getOrThrow<string>('STORAGE_PATH'),
-            imagePath,
+            filePath,
+        );
+
+        const thumbnailDir = path.join(
+            config.getOrThrow<string>('STORAGE_PATH'),
+            userId,
+            'thumbnails',
         );
 
         const sizes = [
@@ -21,24 +48,42 @@ export const generateThumbnails = async (
         ];
 
         const thumbnailPaths = [];
+        const ext = path.extname(filePath).toLowerCase();
+        const isVideo = VIDEO_EXTENSIONS.includes(ext);
+
+        if (isVideo) {
+            const screenshotName = `screenshot_${fileId}.jpg`;
+
+            await new Promise((resolve, reject) => {
+                ffmpeg(fullFilePath)
+                    .on('end', resolve)
+                    .on('error', reject)
+                    .screenshots({
+                        timestamps: ['50%'],
+                        filename: screenshotName,
+                        folder: thumbnailDir,
+                        size: '100%',
+                    });
+            });
+
+            fullFilePath = path.join(thumbnailDir, screenshotName);
+        }
 
         for (const size of sizes) {
             const thumbnailPath = path.join(
-                config.getOrThrow<string>('STORAGE_PATH'),
-                userId,
-                'thumbnails',
-                `${size.name}_${fileId}${path.extname(imagePath)}`,
+                thumbnailDir,
+                isVideo
+                    ? `${size.name}_${fileId}.jpg`
+                    : `${size.name}_${fileId}${path.extname(filePath)}`,
             );
 
-            await sharp(fullImagePath).resize(size.width).toFile(thumbnailPath);
+            await sharp(fullFilePath).resize(size.width).toFile(thumbnailPath);
 
-            thumbnailPaths.push(
-                path.join(
-                    userId,
-                    'thumbnails',
-                    `${size.name}_${fileId}${path.extname(imagePath)}`,
-                ),
-            );
+            thumbnailPaths.push(thumbnailPath);
+        }
+
+        if (isVideo) {
+            await fs.promises.rm(path.join(fullFilePath));
         }
 
         return thumbnailPaths;
